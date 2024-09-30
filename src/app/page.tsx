bf3c0282a3040b1c38a31cbe75ffcf02aea631a5
RATACIA <134484195+RATACIA@users.noreply.github.com>
2024-09-30 17:59:02 +0300

    contact section updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
+
 export default function Home() {
   return (
     <div>
@@ -13,6 +15,7 @@ export default function Home() {
       <TapeSection />
       <TestimonialsSection />
       <AboutSection />
+      <ContactSection />
     </div>
   );
 }
